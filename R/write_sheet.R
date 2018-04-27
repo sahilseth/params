2fb44e2dee32ff4a30c2856cc3836fbbe5f48838
Sahil Seth <sahiilseth@gmail.com>
2018-04-27 18:12:55 -0500

    adding a function to write list of DF

--- R/write_sheet.R
+++ R/write_sheet.R
@@ -21,9 +21,36 @@ write_sheet <- function(x, file, ext, ...){
     }
     openxlsx::write.xlsx(x, file = file, colNames = TRUE, ...)
 
+    if(type == "table"){
+      if(!is.list(x))
+        stop("x is not a list!")
+
+      message("write to excel...")
+      wb <- createWorkbook()
+      sheets <- lapply(names(x), addWorksheet, wb = wb, gridLines = F)
+      tmp = map2(names(x), lst, writeDataTable, wb = wb, bandedRows = F, tableStyle = "TableStyleLight1")
+      saveWorkbook(wb, file, overwrite = T)
+    }
+
   }else{
     stop("Sorry write_sheet does not recognize this file format: ", ext,
          " please use tsv, csv or xlsx")
   }
 
 }
+
+
+
+write_sheets <- function(lst, file){
+  wb <- createWorkbook()
+  sheets <- lapply(names(lst), addWorksheet, wb = wb, gridLines = F)
+  tmp = map2(names(lst), lst, writeDataTable, wb = wb, bandedRows = F, tableStyle = "TableStyleLight1")
+  saveWorkbook(wb, file, overwrite = T)
+
+}
+
+
+
+
+
+# END
